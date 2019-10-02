d9e11bc4e26878517c4f5788f5a4daf48d129e5b
Jimmy S. Emmanuel <jsemmanuel@gmail.com>
2019-10-02 13:03:48 -0400

    Fixed

--- Dna.js
+++ Dna.js
@@ -1,10 +1,10 @@
 class Dna {
-  constructor(maxsiz) {
+  constructor(geneSize) {
     // The genetic sequence
     this.genes = [];
     this.fitness = 0;
 
-    for (let i = 0; i < maxsize; i++) {
+    for (let i = 0; i < geneSize; i++) {
       this.genes[i] = newChar(); // Pick from range of chars
     }
   }
